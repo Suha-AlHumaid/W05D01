02fae296ec43419ad0aeb80bac4a3ceaee99ca9f
Suha-AlHumaid <Suha.Saleh.elq.js@tuwaiq.edu.sa>
2021-11-17 21:36:00 +0300

    get all data

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 const express = require("express"); //to create express app
 const app = express(); //call express
 const PORT = 5000; //set port best praktise in capital letter
-app.use(express.json()) //Json data
+app.use(express.json()); //Json data
 
+let toDos = [
+  { name: "Sleep", isDele: false, id: 0 }, //elem.name
+  { name: "Eat", isDele: false, id: 1 }, //elem
+  { name: "sleep Again", isDele: false, id: 2 }, //elem
+];
+
+///show todos data
+app.get("/todos", (req, res) => {
+  res.status(200).json(toDos);
+});
 
 ////////////////////////////////////////////////////
 app.listen(PORT, () => {
   console.log(`server is running ${PORT}`);
 });
-
